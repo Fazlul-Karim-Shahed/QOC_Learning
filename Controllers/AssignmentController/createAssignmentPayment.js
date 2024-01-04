a4927aaa1368ed629219d65373f8c5be1a97dc05
Fazlul-Karim-Shahed <fazlul.karim1@northsouth.edu>
2024-01-05 02:18:20 +0600

    payment update

--- Controllers/AssignmentController/createAssignmentPayment.js
+++ Controllers/AssignmentController/createAssignmentPayment.js
@@ -2,6 +2,7 @@
 const { default: axios } = require('axios')
 const formData = require('form-data')
 const generateUniqueId = require('generate-unique-id')
+const { StudentModel } = require('../../Models/StudentModel')
 // const axios = require('axios').default
 // const generateUniqueId = require('generate-unique-id')
 // const { Orders } = require('../../../Models/OrderModel')
@@ -10,7 +11,7 @@ const generateUniqueId = require('generate-unique-id')
 
 const createAssignmentPayment = async (req, res) => {
 
-    let data = req.body
+    let data = await StudentModel({ _id: req.body.studentId })
 
     const payData = {
 
@@ -18,53 +19,54 @@ const createAssignmentPayment = async (req, res) => {
         store_passwd: 'shahe6596af62b67e9@ssl',
         total_amount: 500,
         currency: 'BDT',
-        // tran_id: generateUniqueId({ length: 35, useNumbers: true, useLetters: true }),
         tran_id: generateUniqueId({ length: 20, useNumbers: true, useLetters: true }),
         product_category: 'online_service',
-        success_url: 'https://onu-project-38acc.firebaseapp.com/success',
-        fail_url: 'https://onu-project-38acc.firebaseapp.com/fail',
-        cancel_url: 'https://onu-project-38acc.firebaseapp.com/cancel',
+        success_url: 'https://qoc-learning.web.app/success',
+        fail_url: 'https://qoc-learning.web.app/fail',
+        cancel_url: 'https://qoc-learning.web.app/cancel',
         ipn_url: 'https://qoc-learning.onrender.com/api/assignment/payment/ipn',
-        // ipn_url: 'http://localhost:4002/api/assignment/payment/ipn',
 
 
         // EMI Transaction
         emi_option: 0,
 
         //Customer Information
-        cus_name: 'data.name',
-        cus_email: 'data.email',
-        cus_add1: 'data.address',
-        cus_city: 'data.city',
-        cus_postcode: 'data.postCode',
-        cus_country: 'Bangladesh',
-        cus_phone: 'data.phone',
+        cus_name: data.username ? data.username : 'Unknown',
+        cus_email: data.email ? data.email : 'unknown@gmail.com',
+        cus_add1: data.address ? data.address : 'Unknown',
+        cus_city: data.city ? data.city : 'Unknown',
+        cus_postcode: data.postCode ? data.postCode : 'Unknown',
+        cus_country: data.country ? data.country : 'Unknown',
+        cus_phone: data.mobile ? data.mobile : 'Unknown',
 
         //Shipping Information
         shipping_method: 'NO',
         num_of_item: 1,
         weight_of_item: 0,
-        logistic_pickup_id: 'nai',
+        logistic_pickup_id: 'none',
         logistic_delivery_type: '',
 
         //Product Information
         product_name: 'Online Assignment Help',
         product_category: 'Online Service',
         product_profile: 'non-physical-goods',
+
+        //Additional Information
+        value_a: req.body.studentId,
     }
 
 
-    let obj = {}
-
-    
 
     axios.post('https://sandbox.sslcommerz.com/gwprocess/v4/api.php', payData, {
         headers: {
-            "Content-Type": 'multipart/form-data'
+            "content-type": 'multipart/form-data'
         }
     }).then(data => {
-        console.log(data.data);
+        res.send({ message: 'Payment created successfully', error: false, data: data.data })
     })
+    .catch(err => {
+        res.send({ message: 'Something went wrong while creating payment. Please contact with QOC management', error: true, data: err.message })
+    }) 
 
 
 
