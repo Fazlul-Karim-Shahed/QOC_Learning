d31205ffea5f78e91a7abd6f6ad85d867a14c287
Fazlul-Karim-Shahed <fazlul.karim1@northsouth.edu>
2024-01-05 02:34:00 +0600

    updated

--- Controllers/AssignmentController/ipn.js
+++ Controllers/AssignmentController/ipn.js
@@ -4,10 +4,12 @@ const { StudentModel } = require("../../Models/StudentModel")
 const ipn = async (req, res) => {
 
     let data = req.body
+    console.log('id: ', req.body.value_a)
 
     if (data.status === 'VALID') {
 
         let student = await StudentModel({ _id: data.value_a })
+        console
 
         student['assignment'] = {
             isPremium: true,
@@ -16,14 +18,17 @@ const ipn = async (req, res) => {
         }
 
         student.save().then(data => {
+            console.log('Data: ', data)
             res.send({ message: `Transaction status: ${data.status}. Assignment premium service activated till ${new Date(data.assignment.endTime).toLocaleString()}`, error: false, data: data });
         }).catch(err => {
+            console.log('Error: ', err)
             res.send({ message: 'Something went wrong while activating assignment premium service. Please contact with QOC management', error: true, data: err.message });
         })
 
 
     }
     else {
+        console.log({ message: 'Transaction status: ' + data.status})
         res.send({ message: 'Transaction status: ' + data.status, error: true })
     }
 
