295788b515a75655a810a50a9c7b8214165efb46
Fazlul-Karim-Shahed <fazlul.karim1@northsouth.edu>
2024-01-05 23:02:51 +0600

    submit assignment answer

---
+++ Controllers/AssignmentController/submitSolution.js
@@ -0,0 +1,71 @@
+
+const fs = require('fs')
+const { AssignmentModel } = require('../../Models/AssignmentModel')
+const _ = require('lodash')
+const { IncomingForm } = require('formidable')
+
+
+const submitSolution = async (req, res) => {
+
+    let form = new IncomingForm()
+    form.keepExtensions = true
+
+    form.parse(req, (err, fields, files) => {
+
+        if (err) {
+
+            res.send({ message: 'Assignment upload failed', error: true })
+        }
+        else {
+
+            // console.log(files)
+
+            if (files && Object.keys(files).length > 0) {
+
+                let x = new Promise(resolve => {
+
+                    fs.readFile(files['answer'][0].filepath, (err, data) => {
+
+                        resolve({
+                            data: data,
+                            contentType: files['answer'][0].mimetype,
+                            name: files['answer'][0].originalFilename,
+                        })
+
+                    })
+                })
+
+
+                x.then(data => {
+
+                    AssignmentModel.updateOne({ _id: req.params.assignmentId }, { $push: { answer: data } }).then(data => {
+
+                        res.send({ message: 'Answer submitted successfully', error: false, data: data })
+
+                    }).catch(err => {
+
+                        res.send({ message: 'Answer submission failed', error: true, data: err.message })
+                    })
+
+                })
+
+
+
+            }
+            else {
+
+                res.send({ message: 'Answer submission failed. File must needed', error: true })
+
+            }
+
+        }
+
+
+    })
+
+
+}
+
+
+module.exports.submitSolution = submitSolution
+
--- Models/AssignmentModel.js
+++ Models/AssignmentModel.js
@@ -3,7 +3,7 @@
 const { model, Schema } = require('mongoose')
 
 const AssignmentModel = model('Assignment', Schema({
-    
+
     studentId: { type: Schema.Types.ObjectId, ref: 'Student', required: true },
     title: { type: String, required: true },
     assignment: { data: Buffer, contentType: String, type: Object, name: String },
