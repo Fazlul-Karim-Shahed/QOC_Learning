aa61986009c75dcad73e21c1926d3fa408f773b7
Fazlul-Karim-Shahed <fazlul.karim1@northsouth.edu>
2024-01-15 02:08:59 +0600

    bug fix 1

--- Controllers/BatchController/joiningBatchIpn.js
+++ Controllers/BatchController/joiningBatchIpn.js
@@ -1,5 +1,6 @@
 
 
+const { default: mongoose } = require("mongoose")
 const { BatchModel } = require("../../Models/BatchModel")
 
 
@@ -9,17 +10,19 @@ const joiningBatchIpn = async (req, res) => {
 
     if (data.status === 'VALID') {
 
-        let batch = await BatchModel.findOne({ _id: data.value_a })
+        BatchModel.updateOne({ _id: data.value_a }, {
+            $push: {
+                enrolledStudents: {
+                    studentId: new mongoose.Types.ObjectId(data.value_b),
+                    createdAt: new Date().toLocaleString(),
+                    transaction: data.tran_id,
+                }
+            }
+        }).then(data => {
 
-        batch['enrolledStudents'] = [...batch.enrolledStudents, {
-            studentId: data.value_b,
-            createdAt: new Date().toLocaleString(),
-            transaction: data.tran_id,
-        }]
-
-        batch.save().then(data => {
             console.log(data)
             res.send({ message: `Transaction status: ${data.status}. batch premium service activated till ${new Date(data.batch.endTime).toLocaleString()}`, error: false, data: data });
+
         }).catch(err => {
             console.log(err)
             res.send({ message: 'Something went wrong while activating batch premium service. Please contact with QOC management', error: true, data: err.message });
