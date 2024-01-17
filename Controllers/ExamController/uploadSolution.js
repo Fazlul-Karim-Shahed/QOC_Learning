c61ddf751fcd0ebca14aeec6f0317a0d00c3a71c
Fazlul-Karim-Shahed <fazlul.karim1@northsouth.edu>
2024-01-17 18:11:52 +0600

    updated

---
+++ Controllers/ExamController/uploadSolution.js
@@ -0,0 +1,62 @@
+const fs = require("fs");
+const { ExamModel } = require("../../Models/ExamModel");
+const _ = require("lodash");
+const { IncomingForm } = require("formidable");
+
+const uploadSolution = async (req, res) => {
+
+  let form = new IncomingForm();
+  form.keepExtensions = true;
+
+  let exam = await ExamModel.findOne({ _id: req.params.examId });
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      res.send({ message: "solution upload failed", error: true });
+    } else {
+      if (files && Object.keys(files).length > 0) {
+
+        console.log(files)
+
+
+        let x = new Promise((resolve) => {
+          fs.readFile(files["solution"][0].filepath, (err, data) => {
+            resolve({
+              data: data,
+              contentType: files["solution"][0].mimetype,
+              name: files["solution"][0].originalFilename,
+            });
+          });
+        });
+
+        x.then((data) => {
+          exam["solution"] = data;
+
+          exam
+            .save()
+            .then((data) => {
+              res.send({
+                message: "solution created successfully",
+                error: false,
+                data: data,
+              });
+            })
+            .catch((err) => {
+              res.send({
+                message: "solution creation failed",
+                error: true,
+                data: err.message,
+              });
+            });
+        });
+      } else {
+        res.send({
+          message: "solution creation failed. File must needed",
+          error: true,
+        });
+      }
+    }
+  });
+};
+
+module.exports.uploadSolution = uploadSolution;
