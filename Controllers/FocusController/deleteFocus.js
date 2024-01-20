fefb8db33d1adc96c2fefb77cc14532c763764b1
Fazlul-Karim-Shahed <fazlul.karim1@northsouth.edu>
2024-01-21 04:47:29 +0600

    focus

---
+++ Controllers/FocusController/deleteFocus.js
@@ -0,0 +1,19 @@
+
+
+const { FocusModel } = require('../../Models/FocusModel')
+
+const deleteFocus = async (req, res) => {
+
+    let focus = await FocusModel.deleteOne({_id: req.params.focusId})
+
+    if (focus) {
+
+        res.status(200).send({ message: 'Deleted', error: false, data: focus })
+    }
+    else {
+        res.send({ message: 'Not delete', error: true })
+    }
+
+}
+
+module.exports.deleteFocus = deleteFocus
