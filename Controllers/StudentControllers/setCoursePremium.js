26f018521dc8961fab4d96bac479a7f247d54767
Fazlul-Karim-Shahed <fazlul.karim1@northsouth.edu>
2024-01-25 01:46:08 +0600

    course payment

---
+++ Controllers/StudentControllers/setCoursePremium.js
@@ -0,0 +1,31 @@
+
+const { StudentModel } = require("../../Models/StudentModel")
+
+
+const setCoursePremium = async (req, res) => {
+
+    let student = await StudentModel.findOne({ _id: req.params.studentId })
+
+    if (student) {
+
+        student['course'] = {
+            isPremium: true,
+            startTime: new Date().toLocaleString(),
+            endTime: new Date(new Date().getTime() + 15 * 24 * 60 * 60 * 1000).toLocaleString(),
+        }
+
+        student.save().then(data => {
+            res.send({ message: 'student is assigned as Premium', error: false, data: data })
+        })
+            .catch(err => {
+                res.send({ message: 'Failed to assigned student as Premium', error: true, data: err.message })
+
+            })
+    }
+    else {
+        res.status(404).send({ message: 'student not found', error: true })
+    }
+
+}
+
+module.exports.setCoursePremium = setCoursePremium
