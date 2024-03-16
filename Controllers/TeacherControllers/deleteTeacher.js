da88e235246ad9c3c56252132b28a2f07f7cb691
Fazlul-Karim-Shahed <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>
2024-03-17 02:36:36 +0600

    fix

--- Controllers/TeacherControllers/deleteTeacher.js
+++ Controllers/TeacherControllers/deleteTeacher.js
@@ -3,11 +3,14 @@
 const { TeacherModel } = require('../../Models/TeacherModel')
 const { TuitionModel } = require('../../Models/TuitionModel')
 const { TransactionModel } = require('../../Models/TransactionModel')
+const { BatchModel } = require('../../Models/BatchModel')
 
 const deleteTeacher = async (req, res) => {
 
     let transaction = await TransactionModel.deleteOne({ "userInfo.userId": req.params.teacherId })
     let tuition = await TuitionModel.deleteOne({ confirmedTeacherId: req.params.teacherId })
+    let batch = await BatchModel.deleteOne({ teacherId: req.params.teacherId })
+
 
     TeacherModel.deleteOne({ _id: req.params.teacherId }).then(data => {
          res.status(200).send({ message: 'Teacher Deleted', error: false, data: data })
