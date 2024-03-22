3043c7e3996259df63ecb9baef29f71213528298
Fazlul-Karim-Shahed <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>
2024-03-22 15:05:39 +0600

    bug fix

--- Controllers/BatchController/createJoiningBatchPayment.js
+++ Controllers/BatchController/createJoiningBatchPayment.js
@@ -6,6 +6,7 @@ const generateUniqueId = require('generate-unique-id')
 const { BatchModel } = require('../../Models/BatchModel')
 const { StudentModel } = require('../../Models/StudentModel')
 const { TransactionModel } = require('../../Models/TransactionModel')
+const { default: mongoose } = require('mongoose')
 
 
 const createJoiningBatchPayment = async (req, res) => {
@@ -14,59 +15,90 @@ const createJoiningBatchPayment = async (req, res) => {
     let batch = await BatchModel.findOne({ _id: req.body.batchId })
     let student = await StudentModel.findOne({ _id: req.body.studentId })
 
-    axios.post(process.env.bkash_createPaymentApi, {
-        mode: '0000',
-        payerReference: batch._id,
-        callbackURL: 'https://api.qoclearning.com/api/batch/join/payment/ipn',
-        amount: String(batch.fees),
-        currency: 'BDT',
-        intent: 'sale'
-    }, {
-        headers: {
-            "Content-Type": 'application/json',
-            "Accept": "application/json",
-            "Authorization": req.id_token,
-            'X-App-Key': process.env.bkash_app_key
-        }
-    }).then(data => {
+    // if (batch.enrolledStudents.includes(new mongoose.Types.ObjectId(student._id))) {
 
-        if (data.data.statusMessage === "Successful") {
+    //     res.send({ message: 'You already applied this job', error: true })
 
-            TransactionModel.create({
+// }
+    
+    if (batch.enrolledStudents.filter(e => e.studentId.toString() === student._id.toString()).length > 0) {
+        res.send({ message: 'You already enrolled in this batch', error: true })
+    }
+    else {
+        if (String(batch.fees) === '0') {
 
-                userInfo: {
-                    userId: student._id,
-                    username: student.username,
-                    mobile: student.mobile,
-                    email: student.email,
-                    role: student.role,
-                },
-                paymentID: data.data.paymentID,
-                id_token: req.id_token,
-                title: 'Batch',
-                amount: batch.fees,
-                payerReference: batch._id
-
-            }).then(tranData => {
-                res.send({ message: 'Payment Initiated', error: false, data: data.data })
+            BatchModel.updateOne({ _id: batch._id }, {
+                $push: {
+                    enrolledStudents: {
+                        studentId: student._id,
+                        createdAt: new Date().toLocaleString(),
+                    }
+                }
+            }).then(data => {
+                res.send({ message: 'Enrolled Successfully', error: false, data: data })
             }).catch(err => {
-                // console.log(err)
-                res.send({ message: 'Something went wrong while creating transaction model', error: true, data: err });
+                res.send({ message: 'Something went wrong while enrolling', error: true, data: err })
             })
-
 
         }
         else {
-            // console.log(data.data)
-            res.send({ message: `Something went wrong while initiating bkash payment - ${data.data.statusMessage}`, error: true, data: data.data });
+            axios.post(process.env.bkash_createPaymentApi, {
+                mode: '0000',
+                payerReference: batch._id,
+                callbackURL: 'https://api.qoclearning.com/api/batch/join/payment/ipn',
+                amount: String(batch.fees),
+                currency: 'BDT',
+                intent: 'sale'
+            }, {
+                headers: {
+                    "Content-Type": 'application/json',
+                    "Accept": "application/json",
+                    "Authorization": req.id_token,
+                    'X-App-Key': process.env.bkash_app_key
+                }
+            }).then(data => {
+
+                if (data.data.statusMessage === "Successful") {
+
+                    TransactionModel.create({
+
+                        userInfo: {
+                            userId: student._id,
+                            username: student.username,
+                            mobile: student.mobile,
+                            email: student.email,
+                            role: student.role,
+                        },
+                        paymentID: data.data.paymentID,
+                        id_token: req.id_token,
+                        title: 'Batch',
+                        amount: batch.fees,
+                        payerReference: batch._id
+
+                    }).then(tranData => {
+                        res.send({ message: 'Payment Initiated', error: false, data: data.data })
+                    }).catch(err => {
+                        // console.log(err)
+                        res.send({ message: 'Something went wrong while creating transaction model', error: true, data: err });
+                    })
+
+
+                }
+                else {
+                    // console.log(data.data)
+                    res.send({ message: `Something went wrong while initiating bkash payment - ${data.data.statusMessage}`, error: true, data: data.data });
+                }
+
+            }).catch(err => {
+
+                // console.log(err)
+                res.send({ message: 'Something went wrong while initiating bkash payment', error: true, data: err });
+
+            })
+
         }
+    }
 
-    }).catch(err => {
-
-        // console.log(err)
-        res.send({ message: 'Something went wrong while initiating bkash payment', error: true, data: err });
-
-    })
 
 
 
