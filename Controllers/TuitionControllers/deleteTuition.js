f6ebd6305b08b3945c77cc7d77bbcd1e35318dca
Fazlul-Karim-Shahed <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>
2024-04-15 22:08:23 +0600

    update

--- Controllers/TuitionControllers/deleteTuition.js
+++ Controllers/TuitionControllers/deleteTuition.js
@@ -1,7 +1,18 @@
+const { TuitionModel } = require("../../Models/TuitionModel")
 
 
 
 const deleteTuition = async (req, res) => {
+
+    let tuition = await TuitionModel.deleteOne({ _id: req.params.tuitionId })
+
+    if (tuition) {
+
+        res.status(200).send({ message: 'Tuition Deleted', error: false, data: tuition })
+    }
+    else {
+        res.send({ message: 'Not delete', error: true })
+    }
 
 
 
